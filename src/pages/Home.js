b1ae9ff1f43b8e825266a47204b7c005610b1c90
Oscar Chiriboga <oscarmchiriboga@gmail.com>
2021-05-21 09:22:01 -0400

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="container">
+            <h1>About me</h1>
+                <hr/>
+                    <div className="container-2">
+                    <h2>Oscar Chiriboga</h2>
+                    <p>A student currently enrolled in Rutgers Coding Bootcamp Full Stack web development gaining valuable skills
+              such as HTML5, CSS3, JavaScript, Jquery, Node.js, Heroku, User Authentication, Git, GitHub, APIs, AJAX,
+              MongoDB, React.</p>
+                    <p></p>
+                </div>
+            
+        </div>
+    );
+}
+
+export default Home;
