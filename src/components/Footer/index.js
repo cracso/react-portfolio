d42daa85763df00385f0cf90df5647de0c32df06
Oscar Chiriboga <oscarmchiriboga@gmail.com>
2021-05-23 00:53:32 -0400

    updating

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
+
+function Footer() {
+    return (
+        <h4 class="mx-auto">&copy; Oscar Chiriboga</h4>
+    );
+}
+
+export default Footer;
